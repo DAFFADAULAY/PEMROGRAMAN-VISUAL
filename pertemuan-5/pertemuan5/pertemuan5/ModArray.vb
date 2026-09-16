d8e5db2ccfe2caa2eb7244dc620d096e613d8b52
DAFFADAULAY <daffadaulay1999@gmail.com>
2026-09-16 12:12:54 +0700

    Add files via upload

---
+++ pertemuan-5/pertemuan5/pertemuan5/ModArray.vb
@@ -0,0 +1,15 @@
+﻿Module ModArray
+    Public Sub TampilkanPesan(nama As String)
+        MessageBox.Show("Haloo " & nama)
+    End Sub
+
+    Function Hitung(panjang As Integer, lebar As Integer) As Integer
+        If panjang <= 50 Then
+            Return 2 * (panjang + lebar)
+        Else
+            Return panjang * lebar
+        End If
+    End Function
+
+    Public nilai() As Integer = {50, 60, 70, 80, 90, 100}
+End Module
